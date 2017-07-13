73fdf1c16575e3b80fadd90e60594d1ab4d491e4
Eugene Piven <thepegasd@gmail.com>
2017-07-13 21:21:04 +0300

    Remove .idea, fix formatting

--- lib/puppet/string2confl.rb
+++ lib/puppet/string2confl.rb
@@ -10,12 +10,12 @@ module Puppet
         @data = JSON.parse json
       end
     end
-    my_path = (File.dirname(File.expand_path(__FILE__)))
+    my_path     = (File.dirname(File.expand_path(__FILE__)))
     DEFAULT_ERB = File.read(my_path + '/string2confl/reference.html.erb')
-    def self.render_erb(json, erb=String2confl::DEFAULT_ERB)
-      erb = ERB.new(erb,0,'-')
-      print erb.def_class(Puppet::String2confl::ErbPassVal_, 'render()').new(json).render()
 
+    def self.render_erb(json, erb=String2confl::DEFAULT_ERB)
+      erb = ERB.new(erb, 0, '-')
+      print erb.def_class(Puppet::String2confl::ErbPassVal_, 'render()').new(json).render()
     end
   end
 end
