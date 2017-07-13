a79fc3e7754d9563521b60fc46de2907e42cd938
Vladimir Tyshkevich <vtyshkevich@iponweb.net>
2017-07-13 18:54:43 +0300

    Initial working commit

--- lib/puppet/string2confl.rb
+++ lib/puppet/string2confl.rb
@@ -1,7 +1,21 @@
 require "puppet/string2confl/version"
+require 'erb'
+require 'json'
+
 
 module Puppet
   module String2confl
-    # Your code goes here...
+    class ErbPassVal_
+      def initialize(json)
+        @data = JSON.parse json
+      end
+    end
+    my_path = (File.dirname(File.expand_path(__FILE__)))
+    DEFAULT_ERB = File.read(my_path + '/string2confl/reference.html.erb')
+    def self.render_erb(json, erb=String2confl::DEFAULT_ERB)
+      erb = ERB.new(erb)
+      print erb.def_class(Puppet::String2confl::ErbPassVal_, 'render()').new(json).render()
+
+    end
   end
 end
